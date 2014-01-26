0dd5730d9b46a6ffcd7b28e3133f4f4282c510a5
James Collings <james@jclabs.co.uk>
2014-01-26 17:16:34 +0000

    use AUTH_SALT if no $key provided, cheers @nerdworker

--- modules/EncryptLib.php
+++ modules/EncryptLib.php
@@ -14,8 +14,13 @@ class JC_EncryptLib{
      * @return string
      */
 	public function encrypt($string, $key = ''){
-		if(function_exists('mcrypt_encrypt'))
+		if(function_exists('mcrypt_encrypt')){
+
+            if($key == '' || empty($key)){
+                $key = AUTH_SALT;
+            }
             return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
+        }
         
         return base64_encode($string);
 	}
@@ -30,8 +35,13 @@ class JC_EncryptLib{
      * @return string
      */
 	public function decrypt($string, $key = ''){
-		if(function_exists('mcrypt_encrypt'))
+		if(function_exists('mcrypt_encrypt')){
+
+            if($key == '' || empty($key)){
+                $key = AUTH_SALT;
+            }
             return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($string), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+        }
         
         return base64_decode($string);
 	}
